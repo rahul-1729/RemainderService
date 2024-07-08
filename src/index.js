1b188707e3b2b6b61024b310ef226c78cb5ba3d8
rahul-1729 <rk9763981@gmail.com>
2024-07-08 20:45:20 +0530

    added smtp and configured it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT}=require('./config/serverConfig');
+
+// const{sendBasicEmail}=require('./services/email-service');
+const cron = require('node-cron');
+
+
 const setupAndStartServer=()=>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}))
     app.listen(PORT,()=>{
         console.log(`Server started at ${PORT}`)
+
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'rk9763981@gmail.com',
+        //     "this is a testing email",
+        //     "You have are doing great Rahul , just keep on increasing your focus"
+        // )
+
+        cron.schedule('*/2 * * * * *',()=>{
+        console.log('running a task every two seconds');
+        })
     })
 }
 
